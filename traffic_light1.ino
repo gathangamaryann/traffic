0ddd62ac0de85f5cd5fcd07ccee454f341e5eb9f
gathangamaryann <99389003+gathangamaryann@users.noreply.github.com>
2022-03-29 18:43:33 +0300

    Add files via upload

---
+++ traffic_light1.ino
@@ -0,0 +1,27 @@
+int red = 3;
+int yellow =2;
+int green =1;
+
+void setup(){
+
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+ pinMode(green, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(red, HIGH);
+  delay(9000);
+  digitalWrite;(red, LOW);
+  delay(1000);
+  
+  digitalWrite(yellow, HIGH);
+  delay(5000);
+  digitalWrite;(yellow, LOW);
+  
+  digitalWrite(green, HIGH);
+  delay(9000);
+ digitalWrite(green, LOW); 
+  
+}
+    
